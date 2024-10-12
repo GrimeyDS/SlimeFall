b29dae73c1269e18eb2e905dd64700ab944bd497
SvenDS91 <sven.de.schagt@student.howest.be>
2024-10-12 11:51:56 +0200

    Add game class

--- src/slime_fall/lib/main.dart
+++ src/slime_fall/lib/main.dart
@@ -1,6 +1,9 @@
 import 'package:flame/game.dart';
 import 'package:flutter/material.dart';
+import 'package:slime_fall/Class%20Lib/Game/SlimeFallGame.dart';
 
 void main() {
-  runApp(GameWidget(game: game),);
+  runApp(GameWidget(game: slimeGame),);
 }
+
+final slimeGame = Slimefallgame();
