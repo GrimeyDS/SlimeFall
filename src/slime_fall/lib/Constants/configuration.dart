e087cf54835ae9d9695da48dbff906d37986c994
SvenDS91 <sven.de.schagt@student.howest.be>
2024-10-16 18:57:27 +0200

    Adjust game speed

--- src/slime_fall/lib/Constants/configuration.dart
+++ src/slime_fall/lib/Constants/configuration.dart
@@ -1,17 +1,20 @@
 import 'package:flame/components.dart';
 
 class Config {
-  static Vector2 slimeSize = Vector2(35, 20);
+  // Spike
   static Vector2 spikeSize = Vector2(15, 25);
 
+  // Slime
   static double slimeSensitivity = 225;
+  static Vector2 slimeSize = Vector2(35, 20);
+  static Vector2 velocity = Vector2(0, 500); // How fast the player moves down when not under a platform.
 
-  static const double scrollSpeed = 80.0;
-  static const double backgroundScrollSpeed = 100.0;
+  // Platforms
+  static const double scrollSpeed = 150.0;
   static const double platformHeight = 25.0;
-  static const double platformInterval = 1.4;
+  static const double platformInterval = 1;
   static double platformSpacing = slimeSize.x * 1.3;
 
-  // How fast the player moves down when not under a platform.
-  static Vector2 velocity = Vector2(0, 500);
+  // Background
+  static const double backgroundScrollSpeed = 100.0;
 }
