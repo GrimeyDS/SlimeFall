7f7b54d18d92783bfc9a0af93a8c500cf0aabb71
SvenDS91 <sven.de.schagt@student.howest.be>
2024-10-16 19:40:39 +0200

    Add error handling for gyro stream

--- src/slime_fall/lib/Class Library/game/slime_fall_game.dart
+++ src/slime_fall/lib/Class Library/game/slime_fall_game.dart
@@ -1,6 +1,5 @@
 import 'dart:async';
 import 'package:flame/components.dart';
-import 'package:flame/extensions.dart';
 import 'package:flame/game.dart';
 import 'package:flutter/material.dart';
 import 'package:slime_fall/Class%20Library/components/top_hitbox.dart';
