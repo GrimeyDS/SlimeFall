b4354b9a9c139a31eddbb2e81b867fb666ca620e
SvenDS91 <sven.de.schagt@student.howest.be>
2024-10-17 21:31:49 +0200

    Add dash const value

--- src/slime_fall/lib/Constants/configuration.dart
+++ src/slime_fall/lib/Constants/configuration.dart
@@ -2,19 +2,20 @@ import 'package:flame/components.dart';
 
 class Config {
   // Spike
-  static Vector2 spikeSize = Vector2(10, 20);
+  static final Vector2 spikeSize = Vector2(10, 20);
 
   // Slime
-  static double slimeSensitivity = 225;
-  static Vector2 slimeSize = Vector2(35, 20);
-  static Vector2 slimeSizeFalling = Vector2(20, 35);
-  static Vector2 velocity = Vector2(0, 500); // How fast the player moves down when not under a platform.
+  static const double slimeSensitivity = 225;
+  static final Vector2 slimeSize = Vector2(35, 20);
+  static final Vector2 slimeSizeFalling = Vector2(20, 35);
+  static final Vector2 velocity = Vector2(0, 500); // How fast the player moves down when not under a platform.
+  static const double dashDistance = platformHeight * 2;
 
   // Platforms
   static const double scrollSpeed = 150.0;
   static const double platformHeight = 25.0;
   static const double platformInterval = 1;
-  static double platformSpacing = slimeSize.x * 1.3;
+  static final double platformSpacing = slimeSize.x * 1.3;
 
   // Background
   static const double backgroundScrollSpeed = 100.0;
