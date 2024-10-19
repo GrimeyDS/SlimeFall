6bd94a9f402c2363ba8ac3a47f2a53dd64ff0097
SvenDS91 <sven.de.schagt@student.howest.be>
2024-10-19 12:02:54 +0200

    Add overlay map to game widget

--- src/slime_fall/lib/main.dart
+++ src/slime_fall/lib/main.dart
@@ -1,13 +1,25 @@
 import 'package:flame/flame.dart';
 import 'package:flame/game.dart';
 import 'package:flutter/material.dart';
+import 'package:slime_fall/Class%20Library/overlays/dash_cooldown_overlay.dart';
+import 'package:slime_fall/Constants/configuration.dart';
 import 'package:slime_fall/class%20library/game/slime_fall_game.dart';
 
 final slimeGame = SlimeFallGame();
 
 void main() {
+  //runApp(GameWidget(game: slimeGame),);
+  runApp(MaterialApp(
+    home: Scaffold(
+      body: GameWidget(
+        game: SlimeFallGame(),
+        overlayBuilderMap: { 
+          Config.dashCooldownOverlay: (_, SlimeFallGame game) => DashCooldownOverlay(game: game),
+        }
+      ),
+    )
+  ));
 
-  runApp(GameWidget(game: slimeGame),);
   Flame.device.fullScreen();
   Flame.device.setPortraitUpOnly();
 }
