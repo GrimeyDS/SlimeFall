b6a00f6d0e2318a4cd23b29cff3f41f4f3405b03
SvenDS <sven.de.schagt@student.howest.be>
2024-10-22 14:08:28 +0200

    Add error message to page

--- src/slime_fall/lib/pages/register_page.dart
+++ src/slime_fall/lib/pages/register_page.dart
@@ -1,13 +1,15 @@
 import 'package:flutter/material.dart';
 
 class RegisterPage extends StatelessWidget {
-  final Function(String, String) onRegister; // Callback funtion but with parameters
+  final Function(String, String) onRegister; // Callback function for registering
   final VoidCallback onSwitchToLogin;
+  final String errorMessage;
 
   const RegisterPage({
     super.key,
     required this.onRegister,
     required this.onSwitchToLogin,
+    required this.errorMessage,
   });
 
   @override
@@ -35,6 +37,18 @@ class RegisterPage extends StatelessWidget {
               obscureText: true,
             ),
             const SizedBox(height: 20),
+            // Check if errorMessage is not empty and display the error
+            if (errorMessage.isNotEmpty)
+              Padding(
+                padding: const EdgeInsets.only(bottom: 10),
+                child: Text(
+                  errorMessage,
+                  style: const TextStyle(
+                    color: Colors.red, // Style the error message in red
+                    fontWeight: FontWeight.bold,
+                  ),
+                ),
+              ),
             ElevatedButton(
               onPressed: () {
                 final email = emailController.text;
