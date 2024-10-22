0d68c026cf6b64d4cfba2e3f48e5026291b563d8
SvenDS <sven.de.schagt@student.howest.be>
2024-10-22 14:09:02 +0200

    Implement login and registration

--- src/slime_fall/lib/app_state.dart
+++ src/slime_fall/lib/app_state.dart
@@ -7,33 +7,60 @@ import 'package:slime_fall/game/slime_fall_game.dart';
 import 'package:slime_fall/game/overlays/score_overlay.dart';
 import 'package:slime_fall/main.dart';
 import 'package:slime_fall/game/constants/configuration.dart';
+import 'package:slime_fall/services/authentication/authentication_service.dart';
 
 class AppState extends State<GameApp> {
   bool isLoggedIn = false;
   bool isRegistering = false;
+  bool isLoading = false;
+  String errorMessage = '';
 
-  void handleLogin(String email, String password) {
-    // Add service to login user
-    setState(() {
-      isLoggedIn = true;
-    });
+  final AuthenticationService authService = AuthenticationService();
+
+  Future<void> handleLogin(String email, String password) async {
+    try {
+      bool result = await authService.login(email: email, password: password);
+      if (result) {
+        setState(() {
+          isLoggedIn = true;
+          errorMessage = '';
+        });
+      }
+    }
+    catch (e) {
+      setState(() {
+        errorMessage = e.toString();
+      });
+    }
   }
 
-  void handleRegister(String email, String password) {
-    // Add service to register user
-    setState(() {
-      isLoggedIn = true;
-    });
+  Future<void> handleRegister(String email, String password) async {
+    try {
+      bool result = await authService.register(email: email, password: password);
+      if (result) {
+        setState(() {
+          isRegistering = false;
+          errorMessage = 'Registration successful, please login!';
+        });
+      }
+    }
+    catch (e) {
+      setState(() {
+        errorMessage = e.toString();
+      });
+    }
   }
 
   void switchToRegister() {
     setState(() {
+      errorMessage = '';
       isRegistering = true;
     });
   }
 
   void switchToLogin() {
     setState(() {
+      errorMessage = '';
       isRegistering = false;
     });
   }
@@ -55,14 +82,15 @@ class AppState extends State<GameApp> {
                 )
 
               : isRegistering ?
-
                     RegisterPage(
                       onRegister: handleRegister,
                       onSwitchToLogin: switchToLogin,
+                      errorMessage: errorMessage
                     )
                   : LoginPage(
                       onLogin: handleLogin,
                       onSwitchToRegister: switchToRegister,
+                      errorMessage: errorMessage
                     ),
       ),
     );
