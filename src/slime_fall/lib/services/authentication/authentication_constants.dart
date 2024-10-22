7d6d40fba1f3c8b1124e19981eac3589d444a27f
SvenDS <sven.de.schagt@student.howest.be>
2024-10-22 14:45:27 +0200

    Add constants for authentication messages

---
+++ src/slime_fall/lib/services/authentication/authentication_constants.dart
@@ -0,0 +1,14 @@
+class AuthConstants {
+    static const String unableToLogin = 'Was not able to login user: ';
+    static const String unableToRegister = 'Was not able to register user: ';
+
+    static const String registerSuccess = 'Registration successful, please login!';
+    static const String standardLoginError = 'Could not login, make sure your email and password are correct';
+    static const String standardRegisterError = 'Make sure your email is valid and your password is at least 6 characters long and contains a number and a Capital letter';
+
+    static const String userNotFound = 'User not found';
+    static const String userSession = 'loggedUser';
+
+    static const String internalError = 'internal error';
+    static const String exceptionError = 'Exception: ';
+}
