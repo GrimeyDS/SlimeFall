87b5b56379ae96aa35229db1c89fe9db8925a21c
SvenDS <sven.de.schagt@student.howest.be>
2024-10-22 13:30:05 +0200

    Adjust return type for login and register

--- src/slime_fall/lib/services/authentication/authentication_service_interface.dart
+++ src/slime_fall/lib/services/authentication/authentication_service_interface.dart
@@ -3,6 +3,6 @@ import 'package:slime_fall/services/authentication/user_entity.dart';
 abstract class IAuthenticationService {
   Future<void> initialize();
   UserEntity? get currentUser;
-  Future<void> login({ required String email, required String password});
-  Future<void> register({ required String email, required String password});
+  Future<UserEntity> login({ required String email, required String password});
+  Future<UserEntity> register({ required String email, required String password});
 }
