624438b4cbbab23d117fc8a3deb14a34b9545868
SvenDS <sven.de.schagt@student.howest.be>
2024-10-22 19:29:33 +0200

    Add form constants

--- src/slime_fall/lib/services/authentication/authentication_constants.dart
+++ src/slime_fall/lib/services/authentication/authentication_constants.dart
@@ -11,4 +11,16 @@ class AuthConstants {
 
     static const String internalError = 'internal error';
     static const String exceptionError = 'Exception: ';
+    static const String succes = 'success';
+
+    static const String background = 'assets/images/background.png';
+
+    static const String email = 'Email';
+    static const String enterEmail = 'Enter your email';
+    static const String password = 'Password';
+    static const String enterPassword = 'Enter your password';
+    static const String login = 'Login';
+    static const String register = 'Register';
+    static const String toLoginMessage = 'Already have an account? Login';
+    static const String toRegisterMessage = 'Don\'t have an account? Register';
 }
