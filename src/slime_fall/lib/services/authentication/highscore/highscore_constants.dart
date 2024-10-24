22b1e7ca9286e936f65df1e294288d69ef928591
SvenDS91 <sven.de.schagt@student.howest.be>
2024-10-24 22:27:19 +0200

    Add constants for highscore and overlay

---
+++ src/slime_fall/lib/services/authentication/highscore/highscore_constants.dart
@@ -0,0 +1,13 @@
+class HighscoreConstants {
+    static const String userNotFound = 'User not found';
+    static const String unableToGetScore = 'Error retrieving high score';
+    static const String unableToSaveScore = 'Unable to save highscore';
+
+    static const String dbTable = 'highscores';
+    static const String scoreField = 'score';
+
+    static const String score = 'Score';
+    static const String highScore = 'High Score';
+    static const String gameOver = 'Game Over';
+    static const String retry = 'Retry';
+}
