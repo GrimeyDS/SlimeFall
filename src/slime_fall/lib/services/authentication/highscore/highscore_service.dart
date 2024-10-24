5a6b2937148ad80ab540d54f0747b4a32b1d27df
SvenDS <sven.de.schagt@student.howest.be>
2024-10-24 18:14:30 +0200

    Add functions for save and get highscore

--- src/slime_fall/lib/services/authentication/highscore/highscore_service.dart
+++ src/slime_fall/lib/services/authentication/highscore/highscore_service.dart
@@ -1,15 +1,60 @@
+import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:firebase_core/firebase_core.dart';
 import 'package:slime_fall/firebase_options.dart';
+import 'package:slime_fall/services/authentication/authentication_service.dart';
+import 'package:slime_fall/services/authentication/authentication_service_interface.dart';
 import 'package:slime_fall/services/authentication/highscore/highscore_service_interface.dart';
 
 class HighscoreService implements IHighscoreService {
+  late IAuthenticationService authService;
+  late FirebaseFirestore firestore;
 
   HighscoreService() {
     initialize();
   }
 
-    @override
+  @override
   Future<void> initialize() async {
     await Firebase.initializeApp(options: DefaultFirebaseOptions.currentPlatform);
+    authService = AuthenticationService();
+    firestore = FirebaseFirestore.instance;
+  }
+
+  @override
+  Future<List<int>> getHighscores() async {
+    final user = authService.currentUser;
+    if (user == null) {
+      throw Exception('User not found');
+    }
+
+    List<int> highscores = [];
+
+    try {
+      QuerySnapshot querySnapshot = await firestore.collection(user.id).get();
+      for (var doc in querySnapshot.docs) {
+        highscores.add(doc['score']);
+      }
+    }
+    catch (e) {
+      throw Exception('Unable to get highscores');
+    }
+
+    highscores.sort((a, b) => b.compareTo(a));
+    return highscores.take(5).toList();
+  }
+
+  @override
+  Future<void> saveHighscore(int score) async {
+    final user = authService.currentUser;
+    if (user == null) {
+      throw Exception('User not found');
+    }
+
+    try {
+      await firestore.collection(user.id).add({'score': score});
+    }
+    catch (e) {
+      throw Exception('Unable to save highscore');
+    }
   }
 }
