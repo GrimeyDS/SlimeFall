d88b1aab0ded846241a1cc9e10a7121c75f2b4b2
SvenDS91 <sven.de.schagt@student.howest.be>
2024-10-25 11:29:55 +0200

    Add constant for background value

--- src/slime_fall/lib/game/overlays/start_screen_overlay.dart
+++ src/slime_fall/lib/game/overlays/start_screen_overlay.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:slime_fall/pages/components/game_title.dart';
 import 'package:slime_fall/pages/components/submit_button.dart';
+import 'package:slime_fall/services/authentication/authentication_constants.dart';
 
 class StartScreenOverlay extends StatelessWidget {
   final VoidCallback onStart;
@@ -13,7 +14,7 @@ class StartScreenOverlay extends StatelessWidget {
       children: [
       Positioned.fill(
         child: Image.asset(
-        'assets/images/background.png',
+        AuthConstants.background,
         fit: BoxFit.cover,
         ),
       ),
