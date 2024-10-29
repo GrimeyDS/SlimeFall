ca70213e50bdd52b79a574efb275b66502fff154
SvenDS <sven.de.schagt@student.howest.be>
2024-10-29 18:57:07 +0100

    Add dependency injections

--- src/slime_fall/lib/main.dart
+++ src/slime_fall/lib/main.dart
@@ -1,8 +1,23 @@
 import 'package:flame/flame.dart';
 import 'package:flutter/material.dart';
+import 'package:get_it/get_it.dart';
 import 'package:slime_fall/app_state.dart';
+import 'package:slime_fall/services/authentication/authentication_service.dart';
+import 'package:slime_fall/services/authentication/authentication_service_interface.dart';
+import 'package:slime_fall/services/authentication/highscore/highscore_service.dart';
+import 'package:slime_fall/services/authentication/highscore/highscore_service_interface.dart';
+
+final GetIt getIt = GetIt.instance;
+
+void registerDependencies() {
+  getIt.registerSingleton<IAuthenticationService>(AuthenticationService());
+  getIt.registerSingleton<IHighscoreService>(HighscoreService());
+}
+
 
 void main() {
+  WidgetsFlutterBinding.ensureInitialized();
+  registerDependencies();
   runApp(const GameApp());
   Flame.device.fullScreen();
   Flame.device.setPortraitUpOnly();
